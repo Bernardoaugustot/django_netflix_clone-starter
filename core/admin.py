12aa830b627ec8c3d190d6e934041fb66b5c475b
Bernardo Augusto <39442181+Bernardoaugustot@users.noreply.github.com>
2023-08-08 02:00:06 -0300

    cada vez mais caotico

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
+# from django.contrib import admin: Isso importa o módulo admin fornecido pelo Django,
+#  que é responsável pela criação e gerenciamento do painel de administração.
 from .models import CustomUser,Profile,Movie,Video
-
+# models já é um arquivo do proprio sistema que usamos para preparar o terrreno para essa pagina
 # Register your models here.
 admin.site.register(CustomUser)
+#admin.site.register(CustomUser): Isso registra o modelo CustomUser no painel de .
+# administração do Django. Isso significa que você poderá gerenciar os registros do modelo 
+# CustomUser (provavelmente relacionados a usuários personalizados) diretamente no painel de administração,
+#  em vez de interagir diretamente com o banco de dados.
 admin.site.register(Profile)
 admin.site.register(Movie)
 admin.site.register(Video)
+
+# Esse arquivo esta instanciando as Viwers que seram geradas para ser a area de admin, porem tudo pre-fabricado.
--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid
-
+# models.Model è nosso mano que gerencia bancos de dados, esse velho gordo que vai intermediar com o banco de dados.
 AGE_CHOICES=(
     ('All','All'),
     ('Kids','Kids')
@@ -24,16 +24,30 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name +" "+self.age_limit
-
+#Essa gracinha aqui esta complicando minha vida então vou reescrever e resetar as migrates.
 class Movie(models.Model):
     title:str=models.CharField(max_length=225)
     description:str=models.TextField()
     created =models.DateTimeField(auto_now_add=True)
     uuid=models.UUIDField(default=uuid.uuid4,unique=True)
     type=models.CharField(max_length=10,choices=MOVIE_TYPE)
+    #ManyToManyField ta lincando esssa porra a varios videos diferentes, talvez isso seja eficiente para series?
     videos=models.ManyToManyField('Video')
-    flyer=models.ImageField(upload_to='flyers',blank=True,null=True)
+    #o valor Defaul vai evitar um erro idiota
+    flyer=models.ImageField(upload_to='flyers',blank=True,null=True, default= 'media/flyers/erro.png')
     age_limit=models.CharField(max_length=5,choices=AGE_CHOICES,blank=True,null=True)
+    '''
+
+class Movie(models.Model):
+    title:str=models.CharField(max_length=225)
+    description:str=models.TextField()
+    uuid=models.UUIDField(default=uuid.uuid4,unique=True)
+    created =models.DateTimeField(auto_now_add=True)
+    type=models.CharField(max_length=10,choices=MOVIE_TYPE)
+    file=models.FileField(upload_to='movies')
+    flyer=models.CharField(max_length=2250)
+    age_limit=models.CharField(max_length=5,choices=AGE_CHOICES,blank=True,null=True, default= 'All')
+'''
 
 class Video(models.Model):
     title:str = models.CharField(max_length=225,blank=True,null=True)
